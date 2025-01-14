86ba6ed3ad0ae13ddc47efbfe0288e648bd0699b
probrahim <ibrahimhassani674@gmail.com>
2025-01-14 22:20:00 +0100

    action for api

---
+++ main_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import praw
+
+reddit = praw.Reddit(
+    client_id='',
+    client_secret='',
+    user_agent='',
+    username='',
+    password=''
+)
+
+post_id = ''
+submission = reddit.submission(id=post_id)
+
+submission.comments.replace_more(limit=0)
+
+test_list = ['key']
+
+with open('test_comnt', 'w', encoding='utf-8') as file:
+    count = 0
+
+    for comment in submission.comments.list():
+        if any(i in comment.body.lower() for i in test_list):
+            file.write(comment.body + '\n')
+            count += 1
+            if count >= 5:
+                break
+
